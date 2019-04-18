11ba5de816153697d8613a2fa69c4739b4631081
unknown <shvarpa@gmail.com>
2019-04-18 14:02:35 +0300

    refactored x256fgbg_utils.py

--- gif_for_cli/generate/x256fgbg_utils.py
+++ gif_for_cli/generate/x256fgbg_utils.py
@@ -1,20 +1,16 @@
 from x256 import x256
-
+import heapq
 
 # The max possible distance (i.e. between #000 and #fff)
 _MAX_DISTANCE = 256 ** 2 * 3
 
 
+def _get_closest_color(color,amount=1):
+    possible_colors = enumerate(x256.colors)
+    def mapper(item): return {"distance":x256.__distance(item[1], color),"index":item[0]}
+    colors_and_distances = map(mapper,possible_colors)
+    return heapq.nsmallest(amount,colors_and_distances,key=lambda item:item["distance"])
+
+
 def top_2_colors(r, g, b):
-    c = [r, g, b]
-    best = {'distance': _MAX_DISTANCE, 'index': 1}
-    second = {'distance': _MAX_DISTANCE, 'index': 1}
-
-    for index, item in enumerate(x256.colors):
-        d = x256.__distance(item, c)
-        if(d <= best['distance']):
-            if best:
-                second = best
-            best = {'distance': d, 'index': index}
-
-    return best, second
+    return tuple(_get_closest_color([r, g, b],amount=2))
